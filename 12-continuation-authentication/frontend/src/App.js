781d978472dd44fe1dc4d325bc51a365bee2a9d3
swagfinger <swagfinger@gmail.com>
2023-06-08 06:38:31 +0800

    finished authentication

--- 12-continuation-authentication/frontend/src/App.js
+++ 12-continuation-authentication/frontend/src/App.js
@@ -14,21 +14,27 @@ import Auth from './user/pages/Auth';
 import MainNavigation from './shared/components/Navigation/MainNavigation';
 import { AuthContext } from './shared/context/auth-context';
 
+let logoutTimer;
+
 const App = () => {
   const [token, setToken] = useState(false);
   const [userId, setUserId] = useState();
+  const [tokenExpirationDate, setTokenExpirationDate] = useState();
 
   const login = useCallback((uid, token, expirationDate) => {
     setToken(token);
     setUserId(uid);
-    const tokenExpirationDate =
+    const tknExpirationDate =
       expirationDate || new Date(new Date().getTime() + 1000 * 60 * 60);
+
+    setTokenExpirationDate(tknExpirationDate);
+
     localStorage.setItem(
       'userData',
       JSON.stringify({
         userId: uid,
         token: token,
-        expiration: tokenExpirationDate.toISOString(),
+        expiration: tknExpirationDate.toISOString(),
       })
     );
   }, []);
@@ -36,8 +42,19 @@ const App = () => {
   const logout = useCallback(() => {
     setToken(null);
     setUserId(null);
+    setTokenExpirationDate(null);
     localStorage.removeItem('userData');
   }, []);
+
+  useEffect(() => {
+    if (token && tokenExpirationDate) {
+      const remainingTime =
+        tokenExpirationDate.getTime() - new Date().getTime();
+      logoutTimer = setTimeout(logout, remainingTime);
+    } else {
+      clearTimeout(logoutTimer);
+    }
+  }, [token, logout, tokenExpirationDate]);
 
   useEffect(() => {
     const storedData = JSON.parse(localStorage.getItem('userData'));
